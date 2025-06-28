2c5ab3092cddb896243280356a85cb9ae5b6d932
vishwas-tech29 <rallapallivishwas76@gmail.com>
2025-06-28 16:31:57 +0530

    index 1234567..89abcdef 100644

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,3 +66,20 @@ const initSlider = () => {
 
 window.addEventListener("resize", initSlider);
 window.addEventListener("load", initSlider);
+
+
+
+
+
+
+document.querySelectorAll('.animated-service-card').forEach(card => {
+  card.addEventListener('mousemove', (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width/2;
+    const y = e.clientY - rect.top - rect.height/2;
+    card.style.transform = `rotateY(${x/20}deg) rotateX(${-y/20}deg) scale(1.04)`;
+  });
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = '';
+  });
+});
